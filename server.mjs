6d4aac0673d04f9b64eaf5b5aa236e9519a67f04
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-04-25 15:55:33 -0600

    server.mjs is ready and created conn.mjs ..

---
+++ server.mjs
@@ -0,0 +1,25 @@
+//imports
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/conn.mjs';
+//setups
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middlewares
+app.use(express.json());
+connectDB()
+
+//routes
+app.use('/api/car', carRoutes)
+
+//err  handling middlewares
+app.use((err, _req, res, next) => {
+    res.status(500).json({ msg: err.message });
+  });
+
+//listeners
+app.listen(PORT, () => {
+    console.log(`Server running on Port: ${PORT}`);
+  });
